ba6b21f4db6f17ee604e9fd901f3065e7ebe378c
ElnatnSamuel <elnatansamuel22@gmail.com>
2024-12-13 18:24:57 +0300

    allow

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://vehicle-front-iota.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
